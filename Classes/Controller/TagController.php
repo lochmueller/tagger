6b80d76cbc3e2a2f13a7c11e5856f1233a235727
Tim Lochmüller <Spyker@Tims-MacBook-Pro.local>
2015-10-01 09:24:05 +0200

    Releas first dev snapshot

--- Classes/Controller/TagController.php
+++ Classes/Controller/TagController.php
@@ -1,28 +1,8 @@
 <?php
 
-/* * *************************************************************
- *  Copyright notice
+/**
  *
- *  (c) 2012 Tim Lochmüller
- *  
- *  All rights reserved
- *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- * ************************************************************* */
+ */
 
 namespace HDNET\Tagger\Controller;
 
@@ -52,7 +32,6 @@ class TagController extends ActionController
     public function textcloudAction()
     {
         $tags = $this->getTags();
-        DebuggerUtility::var_dump($tags);
         $this->view->assign('tags', $tags);
     }
 
